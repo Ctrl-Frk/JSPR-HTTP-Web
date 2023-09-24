6f39902533c0c133f99c744827686b46f764c8ca
Viacheslav Baranov <slava_bar@list.ru>
2023-09-25 02:28:03 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import ru.netology.handlers.HandlerForClassic;
+import ru.netology.handlers.StandardHandler;
+
+public class Main {
+    public static final int PORT = 9999;
+    public static final int THREADS = 64;
+
+    public static void main(String[] args) {
+        Server server = new Server(THREADS);
+
+        // добавление хендлеров (обработчиков)
+        server.addHandler("GET", "/index.html", new StandardHandler());
+        server.addHandler("GET", "/spring.svg", new StandardHandler());
+        server.addHandler("GET", "/classic.html", new HandlerForClassic());
+
+        server.start(PORT);
+    }
+}
+
+
